2b552787f1a5e623a60d1148420781757eda61a8
EmmettSamoil <esamoil@telus.net>
2022-02-02 14:09:21 -0700

    Update testing-python.py

--- testing-python.py
+++ testing-python.py
@@ -1,4 +1,8 @@
-one = 1   
+# Interger with the value of 1
+one = 1
+# Interger with the value of 2   
 two = 2
+# Assign three variable the value of 2 + 1
 three = 2 + 1 
+# Print the assaigned value of 3
 print(three)
