1b9e9501853f28744c498f3f8b32ce0b0d2cff31
EmmettSamoil <esamoil@telus.net>
2022-03-15 13:35:50 -0600

    a

--- search.py
+++ search.py
@@ -1,11 +1,26 @@
-list = [1,2,3,4,8,9,4,]
-# // Search an array for an item using the linear search algorithm.
+from re import M
+
+
+nums = [10,30,40,45,70,80,85,90,100]
+words = ["at", "ball", "cat", "dog", "eye", "fish", "good"]
+unsorted = [30, 20, 70, 40, 50, 100, 90]
+# // Search an array for an item using the binary search algorithm.
 # // If item found, return index where found, else return -1.
-def linearSearch() :
-    n = -1
-    for i in list:
-        n += 1
-        if i == 9:
-            return n
+def binarySearch(listName, element) :
+    upperIndex = len(listName) - 1
+    lowerIndex = 0
+    while lowerIndex <= upperIndex:
+        middleIndex = (upperIndex+lowerIndex) // 2
+        if listName[middleIndex] == element:
+            return middleIndex
+        elif element < listName[middleIndex]:
+            upperIndex = middleIndex - 1
+        else:
+            lowerIndex = middleIndex + 1
     return -1
-print(linearSearch())
+
+print(binarySearch(nums, 100))
+print(binarySearch(nums, 75))
+print(binarySearch(words, "fish"))
+print(binarySearch(words, "at"))
+print(binarySearch(unsorted, 70))
