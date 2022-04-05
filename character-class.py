1fb4e945f2c363cd5bcaf60d35fe0818e4470a18
EmmettSamoil <esamoil@telus.net>
2022-04-05 14:13:55 -0600

    a

---
+++ character-class.py
@@ -0,0 +1,32 @@
+class character:
+    #
+    def __init__(self, name, phrase1, phrase2):
+        self.name = name
+        self.phrase1 = phrase1
+        self.phrase2 = phrase2
+        self.level = 0
+    # Speak method
+    def speak(self, phraseNum):
+        if phraseNum == 1:
+            print(self.phrase1)
+        elif phraseNum == 2:
+            print(self.phrase2)
+        else:
+            print("bleh")
+    # setLevel Method
+    def setLevel(self, newLevel):
+        self.level = newLevel
+        print(f"New level: {self.level}")
+
+# main function
+def main():
+    # task 2
+    Matt = character("I am Matt", "I go bowling", "I am good at Wii Sports")
+    Guts = character("I'd rather fight for my life than live it", "Hmph", "But if you disturb me ill kill you")
+    # task 3
+    Matt.speak(1)
+    Guts.setLevel(2)
+    Guts.speak(2)
+# end main()
+
+main()
--- testing-python.py
+++ testing-python.py
@@ -1,18 +1,15 @@
-# Interger with the value of 1
-one = 1
-# Interger with the value of 2   
-two = 2
-# Assign three variable the value of 2 + 1
-three = 2 + 1 
-# Print the assaigned value of 3
-print(three)
+nums = [10, 70, 30, 100, 40, 45, 90, 80, 85]
+words = ["dog","at", "good", "eye", "cat", "ball", "fish"]
 
-#List of Fruits
-fruits = ['apple', 'banana', 'cherry']
-for x in fruits:
-    print(x)
+def bubbleSort(anArray):
+    n = len(anArray)
+    for i in range(n):
+        for j in range(n - 1):
+            if anArray[j] > anArray[j+1]:
+                anArray[j], anArray[j+1] = anArray[j+1], anArray[j]
 
-if three > two:
-    print('three is greater')
-else:
-    print("three is less")
+ 
+bubbleSort(nums)
+bubbleSort(words)
+print(nums)
+print(words)
